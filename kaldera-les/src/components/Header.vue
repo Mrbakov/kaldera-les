e4173ab0452ce7d04662bc13639eb49493af5b80
Mbakov <martin.bakov@adesso.bg>
2019-07-08 14:38:55 +0300

    Add initial components

---
+++ kaldera-les/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1>This is the nav bar</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+  height: 50px;
+  background-color: rgba(99, 63, 10, 0.986);
+}
+</style>
--- kaldera-les/src/router.js
+++ kaldera-les/src/router.js
@@ -1,30 +1,16 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Homepage from "@/views/Homepage.vue";
 
 Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    },
-    {
-      path: "/test",
-      name: "test",
-      component: () =>
-      import("./views/Home.1.vue")
+      name: "homepage",
+      component: Homepage
     }
   ]
 });
