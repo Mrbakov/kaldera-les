c17402c0258fae0b8566814874a7869f3539548e
Mbakov <martin.bakov@adesso.bg>
2019-07-09 18:21:03 +0300

    Add background photo

--- kaldera-les/src/App.vue
+++ kaldera-les/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
+    <div></div>
     <Header />
+    <router-view />
   </div>
 </template>
 
@@ -14,4 +16,24 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+html {
+  height: 100vh;
+  background-image: url("./assets/background-image1.jpg");
+  background-size: cover;
+}
+/* .bg-image {
+
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  filter: blur(3px);
+  -webkit-filter: blur(3px);
+  height: 100vh;
+  width: 100vh;
+  /* Center and scale the image nicely */
+/* background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover; */
+/* }  */
+</style>
--- kaldera-les/src/views/Homepage.vue
+++ kaldera-les/src/views/Homepage.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div>
+    <h1>This is the homepage</h1>
+  </div>
 </template>
 
 <script></script>
