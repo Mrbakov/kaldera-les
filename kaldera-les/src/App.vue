4f398e50ecf88c587576d678be6aa024ad3c76c3
Mbakov <martin.bakov@adesso.bg>
2019-07-10 11:44:44 +0300

    Remove git merge indicators

--- kaldera-les/src/App.vue
+++ kaldera-les/src/App.vue
@@ -19,30 +19,10 @@ export default {
 <style>
 html {
   height: 100vh;
-<<<<<<< HEAD
-  background-image: url("./assets/background-image1.jpg");
-  background-size: cover;
-}
-/* .bg-image {
-
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  filter: blur(3px);
-  -webkit-filter: blur(3px);
-  height: 100vh;
-  width: 100vh;
-  /* Center and scale the image nicely */
-/* background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover; */
-/* }  */
-=======
   background-image: url(./assets/background-image.jpg);
 }
 body {
   margin: 0;
   padding: 0;
 }
->>>>>>> c13272853e14a57ea1fb2a9d8a898e2c67b87bc9
 </style>
